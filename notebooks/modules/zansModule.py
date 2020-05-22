4962f69daaf585298987b8dfa203b71e997e58a7
Zan <zasonce@gmail.com>
2020-05-22 16:52:37 +0200

    Mortality for each country added

--- notebooks/modules/zansModule.py
+++ notebooks/modules/zansModule.py
@@ -83,7 +83,6 @@ def my_round(x, base=5):
     return base * round(x/base)
 
 def get_age_distribution_array(data_set, country, date):  # works only for multiples of 5, from 1950 - 2020
-        date = myround(date)
         distribution_array = []
         country_pop = data_set[(data_set['Region-Country'] == country) & (data_set["Date"] == date)]
         temp_sum = country_pop.iloc[:, 2:-2]
@@ -129,41 +128,68 @@ def draw_age_dist_for_all(data_set):
 
 def death_count_country_year(country, year):
     col_names = ["Country", "Year", "Area", "Month", "Record Type", "Reliability", "Source Year", "Value", "Value Footnotes"]
+    months_arr = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
     deaths_data = pandas.read_csv('../podatki/unData_deaths_per_month.txt', sep=";", header=0, names=col_names)
 
-    country_data = deaths_data.loc[(deaths_data["Country"] == country) & (deaths_data["Year"] == year)]
+    country_data = deaths_data.loc[(deaths_data["Country"] == country) & (deaths_data["Year"] == str(year))]
     month_death_dict = dict()
 
     for idx, row in country_data.iterrows():
         if row["Month"] != "Total":
             month_death_dict[row["Month"]] = row["Value"]
 
+
+    if month_death_dict == {} and not all(month in month_death_dict for month in months_arr):
+        print("Try another year data missing for this one !" + str(month_death_dict))
+        return None
+    # print("keys " + str(month_death_dict.keys()))
+    month_death_dict = {my_month: month_death_dict[my_month] for my_month in months_arr}
     return month_death_dict
 
 
-def drawBarChartMortality(year1, year2, country): 
+def month_deaths(deaths_dict):
+    if deaths_dict is not None:
+        sum_deadth = 0
+        keys = list(deaths_dict.keys())
+        if len(keys) != 12:
+            print("Dates must contain 12 months !!")
+        
+        else:
+            for key in keys:
+                sum_deadth += deaths_dict[key]
+            avg = sum_deadth / 12.0
+            return avg
+    else:
+        return "(No data)"
+    
+
+def draw_bar_chart_mortality(year1, year2, country): 
     year1 = str(year1)
     year2 = str(year2)
     month_num_dict_1 = death_count_country_year(country, year1)
     month_num_dict_2 = death_count_country_year(country, year2)
-    x_ticks_1 = sorted(month_num_dict_1.keys())
-    x_ticks_2 = sorted(month_num_dict_2.keys())
-    
-    if x_ticks_1 == x_ticks_2:        
-        fig = plt.figure(figsize=(14, 6), dpi= 100, facecolor='w')
-    
-        plt.plot(list(month_num_dict_1.keys()), list(month_num_dict_1.values()), color=(0.3, 0.8, 0.2, 0.70),
-                label=(country + " " + str(year1) + " leto"))
-        plt.plot(list(month_num_dict_2.keys()), list(month_num_dict_2.values()), color='royalblue',
-                label=(country + " " + str(year2) + " leto"))         
-        # plt.gcf().autofmt_xdate()
-        # plt.margins(x=0)
-        plt.xlabel('Months')
-        plt.ylabel('Number of deaths')
-        plt.title(country + ' mortality')
-        plt.legend()
-        plt.show()
-    else:
-        print("Dates do not match")
-        print(x_ticks_1)
-        print(x_ticks_2)
+    if month_num_dict_1 is not None and month_num_dict_2 is not None:
+        x_ticks_1 = sorted(month_num_dict_1.keys())
+        x_ticks_2 = sorted(month_num_dict_2.keys())
+        
+        if x_ticks_1 == x_ticks_2:        
+            fig = plt.figure(figsize=(14, 6), dpi= 100, facecolor='w')
+        
+            plt.plot(list(month_num_dict_1.keys()), list(month_num_dict_1.values()), color=(0.3, 0.8, 0.2, 0.70),
+                    label=(country + " " + str(year1) + " leto"))
+            plt.plot(list(month_num_dict_2.keys()), list(month_num_dict_2.values()), color='royalblue',
+                    label=(country + " " + str(year2) + " leto"))         
+            # plt.gcf().autofmt_xdate()
+            # plt.margins(x=0)
+            plt.xlabel('Months')
+            plt.ylabel('Number of deaths')
+            plt.title(country + ' mortality')
+            plt.legend()
+            plt.show()
+        else:
+            print("Dates do not match")
+            print(x_ticks_1)
+            print(x_ticks_2)
+
+
+
